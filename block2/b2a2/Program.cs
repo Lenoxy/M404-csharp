f13a7b6cce1e1a2c9801bb36a54067d4b0b1463e
Lenoxy <43400616+Lenoxy@users.noreply.github.com>
2019-02-18 11:22:35 +0100

    Lektion vom 18.02

---
+++ block2/b2a2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b2a2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            enum Ampel { Rot, RotGelb, Gelb, Gruen};
+
+        }
+    }
+}
---
+++ block2/b2a5/b2a5/b2a5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b2a5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<byte> eingabeL = new List<byte>();
+            byte eingabe;
+            bool failed = false;
+            for(int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    if (failed)
+                    {
+                        i--;
+                    }
+                    Console.Write("Zahl " + Convert.ToString(i + 1) + ": ");
+                    eingabe = Convert.ToByte(Console.ReadLine());
+                    eingabeL.Add(Convert.ToByte(eingabe));
+                    failed = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Geben Sie eine Zahl zwischen 1 und 256 ein.");
+                    failed = true;
+                }
+            }
+
+        }
+    }
+}
