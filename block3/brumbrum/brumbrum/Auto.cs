e06a44f264bba9d44eefd22895c9a134b80011b1
Lenoxy <43400616+Lenoxy@users.noreply.github.com>
2019-04-01 11:23:28 +0200

    Lektion 01.04

---
+++ block3/brumbrum/brumbrum/Auto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brumbrum
+{
+    class Auto
+    {
+        int geschwindigkeit;
+        int maxGeschwindigkeit;
+        string marke;
+
+
+        public int getGeschwindigkeit()
+        {
+            return geschwindigkeit;
+        }
+
+        public bool setGeschwindigkeit(int inputGeschwindigkeit)
+        {
+            if (inputGeschwindigkeit <= 250)
+            {
+                geschwindigkeit = inputGeschwindigkeit;
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+
+
+    }
+}
---
+++ block3/brumbrum/brumbrum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brumbrum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto a = new Auto();
+            bool z = a.setGeschwindigkeit(210);
+            Console.WriteLine(z + ", " + a.getGeschwindigkeit());
+
+
+            Console.ReadLine();
+        }
+    }
+}
