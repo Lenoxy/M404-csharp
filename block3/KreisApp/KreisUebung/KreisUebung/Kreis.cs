0b47ebca3bdf2e5ddfe95f381a6ddb1b71da4ac0
Lenoxy <43400616+Lenoxy@users.noreply.github.com>
2019-04-15 11:21:45 +0200

    Lektion 15.04

---
+++ block3/KreisApp/KreisUebung/KreisUebung/Kreis.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KreisUebung
+{
+    class Kreis
+    {
+        //Member
+        private double mDurchmesser;
+
+
+        //Konstrutoren
+
+        public Kreis()
+        {
+            setDurchmesser(1);
+        }
+
+        public Kreis(double d)
+        {
+            setDurchmesser(d);
+        }
+
+        //Methoden
+        public double getDurchmesser()
+        {
+            return mDurchmesser;
+        }
+
+        public void setDurchmesser(double d)
+        {
+            if(d > 0)
+            {
+                mDurchmesser = d;
+            }
+            else
+            {
+                Console.WriteLine("Durchmesser muss grösser als 0 sein");
+            }
+
+        }
+
+        public double getUmfang()
+        {
+            return mDurchmesser * Math.PI;
+        }
+
+        public double getFlaeche()
+        {
+            return Math.Pow(mDurchmesser, 2)*Math.PI/4;
+        }
+    }
+
+
+}
---
+++ block3/KreisApp/KreisUebung/KreisUebung/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KreisUebung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kreis a = new Kreis();
+            Console.WriteLine("Geben Sie den Durchmesser Ihres Kreises ein:");
+            a.setDurchmesser(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Fläche: " + Convert.ToString(a.getFlaeche()));
+            Console.WriteLine("Umfang: " + Convert.ToString(a.getUmfang()));
+            Console.ReadKey();
+        }
+    }
+}
